378b37bc2673706327eb7c6fc282db67fab9280f
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-09-26 12:29:27 +0530

    fix: currency trending

--- components/sections/Currency.jsx
+++ components/sections/Currency.jsx
@@ -1,11 +1,10 @@
 import emoji from "react-easy-emoji";
-import {
-  ChevronRightIcon,
-  PlusSmallIcon,
-  MinusSmallIcon,
-} from "@heroicons/react/24/solid";
-import bitCoin from "../../public/images/bitcoin.png";
+import {ChevronRightIcon,PlusSmallIcon,MinusSmallIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+
+import bitCoin from "../../public/images/bitcoin.png";
+import upchart from '../../public/images/up-chart.png'
+import downchart from '../../public/images/down-chart.png'
 
 const Currency = () => {
   return (
@@ -31,16 +30,22 @@ const Currency = () => {
                 <span className="text-gray">Chart</span>
               </div>
               <div className="grid grid-cols-3">
-                <div>
-                  <Image alt="bit" src={bitCoin} />
+                <div className="flex items-center -ml-[2px]">
+                  <Image alt="bit" src={bitCoin} width={22} height={22}/>
+                  <div className="ml-2">
                   Bitcoin
+                  </div>
                 </div>
                 <div className="flex items-center">
                   <PlusSmallIcon
-                    width={25}
-                    className="text-green inline mr-2"
+                    className="text-green"
                   />
+                  <div className="mr-5">
                   $43,180.13
+                  </div>
+                </div>
+                <div>
+                  <Image src={upchart} alt="upchart"/>
                 </div>
               </div>
             </div>
