ee81f1c557e616b93c2ce9c4158e7be32d403fa9
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-10-04 11:17:12 +0530

    feat: gainers & recentCoins

--- components/CoinList/CoinRow.jsx
+++ components/CoinList/CoinRow.jsx
@@ -1,5 +1,4 @@
 import {
-  ChevronRightIcon,
   PlusSmallIcon,
   MinusSmallIcon,
 } from "@heroicons/react/24/solid";
@@ -9,16 +8,16 @@ import Image from "next/image";
 
 const CoinRow = ({ image, coinName, coinPrice, coinUpTrend }) => {
   return (
-    <div className="grid grid-cols-3 mb-2">
+    <div className="grid grid-cols-3 mb-2 py-2 border-b border-lightgray">
       <div className="flex items-center -ml-[2px]">
         <Image alt="bit" src={image} width={22} draggable="false" height={22} />
         <div className="ml-2">{coinName}</div>
       </div>
       <div className="flex items-center">
         {coinUpTrend ? (
-          <PlusSmallIcon className="text-green" />
+          <PlusSmallIcon className="text-green mr-1" />
         ) : (
-          <MinusSmallIcon className="text-red" />
+          <MinusSmallIcon className="text-red mr-1" />
         )}
 
         <div className="mr-5">{coinPrice}</div>
--- components/sections/Currency.jsx
+++ components/sections/Currency.jsx
@@ -3,6 +3,14 @@ import bitCoin from "../../public/images/bitcoin.png";
 import Etherium from '../../public/images/Etherium.png'
 import Solana from '../../public/images/solana.png'
 import Dogo from '../../public/images/dogo.png'
+import Pappay from '../../public/images/pappay.svg'
+import BitAsia from '../../public/images/bit-asia.png'
+import MoonRock from '../../public/images/moon-rock.png'
+import Ninja from '../../public/images/ninja.png'
+import Meta from '../../public/images/meta.png'
+import Frog from '../../public/images/frog.png'
+import Musk from '../../public/images/musk.png'
+import Share from '../../public/images/2-share.png'
 
 const trendingCoins = [
   {
@@ -30,8 +38,60 @@ const trendingCoins = [
     upTrend: true,
   },
 ];
-const gainersCoins = [];
-const recentCoins = [];
+const gainersCoins = [
+  
+  {
+    image: { Pappay },
+    name: "PAPPAY",
+    price: "$0.00374",
+    upTrend: true,
+  },
+  {
+    image: { BitAsia },
+    name: "BitcoinAsia",
+    price: "$0.20096",
+    upTrend: false,
+  },
+  {
+    image: { MoonRock },
+    name: "MoonRock",
+    price: "$0.04907",
+    upTrend: true,
+  },
+  {
+    image: { Ninja },
+    name: "NinjaFloki",
+    price: "$0.000123",
+    upTrend: true,
+  },
+];
+const recentCoins = [
+  
+  {
+    image: { Meta },
+    name: "Metacraft",
+    price: "$0.00608",
+    upTrend: false,
+  },
+  {
+    image: { Frog  },
+    name: "Frog",
+    price: "$0.05875",
+    upTrend: false,
+  },
+  {
+    image: { Musk },
+    name: "MuskDoge",
+    price: "$0.04041",
+    upTrend: true,
+  },
+  {
+    image: { Share },
+    name: "2 SHARE",
+    price: "$1,366.24",
+    upTrend: true,
+  },
+];
 
 const Currency = () => {
   return (
